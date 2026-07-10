42802124167465652c607429d9c76dfe30268606
Gautam Gupta <gupta08gautam@gmail.com>
2026-07-10 23:37:38 +0530

    Practice

---
+++ Arrays/RemoveDuplicatesOptimal.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class RemoveDuplicatesOptimal {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+
+        System.out.print("Enter elements of array: \n");
+        for(int i = 0; i < n; i++){
+
+        }
+    }
+}
