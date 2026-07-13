4f7738e4b283528ba3dedb4efef531a7e5b58a33
Gautam Gupta <gupta08gautam@gmail.com>
2026-07-13 05:56:28 +0530

    Practice

---
+++ LeetcodeContests/Contest510/ElapsedSecondsBetnTwoTimes.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class ElapsedSecondsBetnTwoTimes {
+    public static void main(String[] arg){
+        String startTime = "01:00:00";
+        String endTime = "01:00:25";
+
+        Solution510 sol = new Solution510();
+        int res = sol.secondsBetweenTimes(startTime, endTime);
+    }
+}
+
+class Solution510{
+    public int secondsBetweenTimes(String startTime, String endTime) {
+        int sec = 0;
+
+        for(int i = 0; i < startTime.length(); i++){
+            if(i == ':'){
+
+            }
+        }
+
+        return sec;
+    }
+}
