ee5ed765565cfb1193b263d446d639ddb037b2c6
Gautam Gupta <gupta08gautam@gmail.com>
2026-07-29 23:30:07 +0530

    Practice

--- SortingAlgos-II/MergeSort.java
+++ SortingAlgos-II/MergeSort.java
@@ -1,2 +1,26 @@
 public class MergeSort {
+    public static void main(String[] args){
+        int [] arr = {1, 3, 2, 3, 1};
+
+        System.out.print("Before merge sort : ");
+        System.out.print(arr);
+
+        mergeSort( arr,0,arr.length -1);
+
+        System.out.print("After merge sort: ");
+        System.out.print(arr);
+    }
+
+    // 1 3 2 5 7 9
+
+    static void mergeSort(int [] arr, int low, int high){
+        if(low == high){
+            return;
+        }
+    }
+
+    static void merge(){
+
+    }
 }
+
