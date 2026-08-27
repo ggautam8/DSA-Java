fbc9caa9c9c022f9ca21a67f0fb6f8428d89e4cf
Gautam Gupta <gupta08gautam@gmail.com>
2026-08-27 19:31:43 +0530

    Practice

---
+++ src/Common/ConvertArrToLL.java
@@ -0,0 +1,15 @@
+package Common;
+
+public class ConvertArrToLL {
+    public Node convert2LL(int[] arr){
+        Node head = new Node(arr[0]);
+        Node mover = head;
+
+        for(int i = 1; i < arr.length; i++){
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover = temp;
+        }
+        return head;
+    }
+}
