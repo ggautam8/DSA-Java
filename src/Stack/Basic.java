7893ba859c0cbbf8ab003afa9df9ac055fd2e9f4
Gautam Gupta <gupta08gautam@gmail.com>
2026-08-30 22:35:08 +0530

    Practice

---
+++ src/Stack/Basic.java
@@ -0,0 +1,15 @@
+package Stack;
+import java.util.*;
+
+class Solution1{
+
+}
+
+public class Basic {
+    public static void main(String[] args){
+        int[] arr = {1, 3, 2, 4};
+
+        Solution1 sol = new Solution1();
+
+    }
+}
