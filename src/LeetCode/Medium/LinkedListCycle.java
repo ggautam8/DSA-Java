c5c3eb99b6ef071f0a8cfc06a8ff9f312a169b92
Gautam Gupta <gupta08gautam@gmail.com>
2026-09-25 11:06:58 +0530

    Practice

---
+++ src/LeetCode/Medium/LinkedListCycle.java
@@ -0,0 +1,43 @@
+import Common.Node;
+
+class Solution141{
+    public boolean detectCycle(Node head){
+        Node slow = head;
+        Node fast = head;
+
+        while(fast!= null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+public class LinkedListCycle {
+    public static void main(String[] args){
+        Solution141 sol = new Solution141();
+
+        Node head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+        Node fourth = new Node(4);
+        Node fifth = new Node(5);
+
+        head.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = fifth;
+        fifth.next = third;
+
+        if(sol.detectCycle(head)){
+            System.out.print("Loop detected in this linked list");
+        }
+        else{
+            System.out.print("No loop detected in this linked list");
+        }
+    }
+}
